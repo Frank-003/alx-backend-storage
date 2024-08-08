0f3eaef1e2b96b5bd9aea5dbe491f4f475e12404
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-08 08:08:32 +0000

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
 --  lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT 
-    band_name As band_name,
-IFNULL(split, 2020) - IFNULL(formed, 0) As lifespan
+    band_name As band_name, IFNULL(split, 2020) - IFNULL(formed, 0) As lifespan
 FROM 
     metal_bands
 WHERE 
