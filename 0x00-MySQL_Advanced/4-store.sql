8f115a601f579b5b410a1e2b27ba4d772f032329
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-08 08:45:07 +0000

    task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,10 +2,8 @@
 CREATE TRIGGER 
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END 
+    WHERE name = NEW.item_name; 
 
 
