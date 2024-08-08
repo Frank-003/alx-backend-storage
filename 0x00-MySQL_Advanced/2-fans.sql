716a985f6925e12b23e9e1bb3c80e3e5b7a07851
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-08 07:03:03 +0000

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,7 @@
--- Ensure you have the metal_bands table imported in your database
 
--- SQL script to rank country origins by the number of fans
 SELECT 
     origin, 
-    SUM(nb_fans) AS total_fans
+    SUM(fans)AS nb_fans
 FROM 
     metal_bands
 GROUP BY 
