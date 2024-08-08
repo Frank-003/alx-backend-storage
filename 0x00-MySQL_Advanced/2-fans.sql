1f2f63d4e3624a39b1fc422f558c26c76bcc7e19
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-08 07:30:01 +0000

    task 0

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
-
+--  ranks country origins of bands, ordered by the number of (non-unique) fans
 SELECT 
     origin As origin, 
-     SUM(fans)AS nb_fans
+SUM(fans)AS nb_fans
 FROM 
     metal_bands
 GROUP BY 
